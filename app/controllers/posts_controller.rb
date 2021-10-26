82c8c4520f09c4418b990f2c22ffe9ca55d0f283
vincent <vincentuong@yahoo.com>
2021-10-25 22:46:00 -0500

    commit tree 31bf71e0bac695f79f54d444bd9e13321111de76

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
-	end
-
-	def show
-		@post = Post.find(params[:id])
-	end
+	  end
+	
+	  def new
+	  end
+	
+	  def create
+		Post.create(title: params[:post][:title], description: params[:post][:description])
+		redirect_to posts_path
+	  end
 end
